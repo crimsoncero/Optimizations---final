89a42d280fd1b8e42b0628be353bef01f2067461
Leeshap98 <127130059+Leeshap98@users.noreply.github.com>
2025-08-30 16:03:28 +0300

    Finished optimizing scripts

--- Assets/Arctic_Castle/Scripts/Input Actions/CharController_Motor.cs
+++ Assets/Arctic_Castle/Scripts/Input Actions/CharController_Motor.cs
@@ -2,59 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharController_Motor : MonoBehaviour {
+public class CharController_Motor : MonoBehaviour
+{
+    public float speed = 2f;
+    public float sensitivity = 10.0f;
+    public bool webGLRightClickRotation = true;
 
-	public float speed = 2f;
-	public float sensitivity = 10.0f;
-	CharacterController character;
-	public GameObject cam;
-	float moveFB, moveLR;
-	float rotX, rotY;
-	public bool webGLRightClickRotation = true;
+    [SerializeField] private CharacterController character;
+    [SerializeField] private Camera cam;
+    private float moveFB, moveLR;
+    private float rotX, rotY;
 
+    void Start()
+    {
+        if (character == null)
+            Debug.LogWarning("CharacterController reference is not assigned!");
+        if (cam == null)
+            Debug.LogWarning("Camera reference is not assigned!");
 
+#if UNITY_EDITOR
+        webGLRightClickRotation = false;
+        sensitivity *= 2;
+#endif
+    }
 
-	void Start(){
-		//LockCursor ();
-		character = GetComponent<CharacterController> ();
-		if (Application.isEditor) {
-			webGLRightClickRotation = false;
-			sensitivity = sensitivity * 2;
-		}
-	}
+    void Update()
+    {
+        moveFB = Input.GetAxis("Horizontal") * speed;
+        moveLR = Input.GetAxis("Vertical") * speed;
 
+        rotX = Input.GetAxis("Mouse X") * sensitivity;
+        rotY = Input.GetAxis("Mouse Y") * sensitivity;
 
+        Vector3 movement = new Vector3(moveFB, -9.8f, moveLR) * Time.deltaTime;
 
-	void Update(){
-		moveFB = Input.GetAxis ("Horizontal") * speed;
-		moveLR = Input.GetAxis ("Vertical") * speed;
+        if (webGLRightClickRotation && Input.GetKey(KeyCode.Mouse0) || !webGLRightClickRotation)
+        {
+            CameraRotation(rotX, rotY);
+        }
 
-		rotX = Input.GetAxis ("Mouse X") * sensitivity;
-		rotY = Input.GetAxis ("Mouse Y") * sensitivity;
+        movement = transform.rotation * movement;
+        if (character != null)
+        {
+            character.Move(movement);
+        }
+    }
 
-		Vector3 movement = new Vector3 (moveFB, -9.8f, moveLR);
+    void CameraRotation(float rotX, float rotY)
+    {
+        transform.Rotate(0, rotX * Time.deltaTime, 0);
 
+        rotY = Mathf.Clamp(rotY, -80f, 80f);
 
-
-		if (webGLRightClickRotation) {
-			if (Input.GetKey (KeyCode.Mouse0)) {
-				CameraRotation (cam, rotX, rotY);
-			}
-		} else if (!webGLRightClickRotation) {
-			CameraRotation (cam, rotX, rotY);
-		}
-
-		movement = transform.rotation * movement;
-		character.Move (movement * Time.deltaTime);
-	}
-
-
-	void CameraRotation(GameObject cam, float rotX, float rotY){		
-		transform.Rotate (0, rotX * Time.deltaTime, 0);
-		cam.transform.Rotate (-rotY * Time.deltaTime, 0, 0);
-	}
-
-
-
-
+        if (cam != null)
+        {
+            cam.transform.Rotate(-rotY * Time.deltaTime, 0, 0);
+        }
+    }
 }
